8dec1b9ab418bc1680ccbcca02659816af5cbe5c
Sreenath Rangana <sreenath@zelarsoft.com>
2021-10-01 17:04:59 +0530

    ss

--- k8/cluster_create.sh
+++
@@ -1,3 +0,0 @@
-k3d cluster create react-app -p "3000:80@loadbalancer"
-kubectl config use-context k3d-react-app
-bash kube.sh
--- k8/kube.sh
+++
@@ -1,7 +0,0 @@
-kubectl apply -f mysql-deployment.yml
-kubectl apply -f mysql-service.yml
-kubectl apply -f backend-deployment.yml
-kubectl apply -f backend-service.yml
-kubectl apply -f frontend-deployment.yml
-kubectl apply -f frontend-service.yml
-kubectl apply -f ingress.yml
